9cfb8a78020f30ed37bdc66210f4c3778f687ceb
Robby Smet <robbysmet13@gmail.com>
2014-05-27 09:29:41 +0200

    Readd

--- AsyncListView/src/com/survivingwithandroid/tutorial/asynclistview/MainActivity.java
+++ AsyncListView/src/com/survivingwithandroid/tutorial/asynclistview/MainActivity.java
@@ -27,6 +27,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
@@ -36,27 +42,66 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 	private SimpleAdapter adpt;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        adpt  = new SimpleAdapter(new ArrayList<Contact>(), this);
-        ListView lView = (ListView) findViewById(R.id.listview);
-        
-        lView.setAdapter(adpt);
-        
-        // Exec async load task
-        (new AsyncListViewLoader()).execute("http://10.0.2.2:8080/JSONServer/rest/ContactWS/get");
-    }
+	private String url = "http://www.json-generator.com/j/clAiobfHaW?indent=4";
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    private class AsyncListViewLoader extends AsyncTask<String, Void, List<Contact>> {
-    	private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
-    	
+		adpt = new SimpleAdapter(new ArrayList<Contact>(), this);
+		ListView lView = (ListView) findViewById(R.id.listview);
+
+		lView.setAdapter(adpt);
+
+		// Exec async load task
+		// (new AsyncListViewLoader()).execute(url);
+
+		RequestQueue rq = Volley.newRequestQueue(this);
+		JsonArrayRequest jReq = new JsonArrayRequest(url,
+				new Response.Listener<JSONArray>() {
+
+					@Override
+					public void onResponse(JSONArray response) {
+						List<Contact> result = new ArrayList<Contact>();
+						
+						for (int i = 0; i < response.length(); i++) {
+							try {
+								result.add(convertContact(response.getJSONObject(i)));
+							} catch (JSONException e) {
+							}
+						}
+						adpt.setItemList(result);
+						adpt.notifyDataSetChanged();
+					}
+				}, new Response.ErrorListener() {
+
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						// Handle error
+
+					}
+				});
+
+		rq.add(jReq);
+	}
+
+	private final Contact convertContact(JSONObject obj) throws JSONException {
+		String name = obj.getString("name");
+		String surname = obj.getString("surname");
+		String email = obj.getString("email");
+		String phoneNum = obj.getString("phoneNum");
+
+		return new Contact(name, surname, email, phoneNum);
+	}
+
+	private class AsyncListViewLoader extends
+			AsyncTask<String, Void, List<Contact>> {
+		private final ProgressDialog dialog = new ProgressDialog(
+				MainActivity.this);
+
 		@Override
-		protected void onPostExecute(List<Contact> result) {			
+		protected void onPostExecute(List<Contact> result) {
 			super.onPostExecute(result);
 			dialog.dismiss();
 			adpt.setItemList(result);
@@ -64,58 +109,44 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		protected void onPreExecute() {		
+		protected void onPreExecute() {
 			super.onPreExecute();
 			dialog.setMessage("Downloading contacts...");
-			dialog.show();			
+			dialog.show();
 		}
 
 		@Override
 		protected List<Contact> doInBackground(String... params) {
 			List<Contact> result = new ArrayList<Contact>();
-			
+
 			try {
 				URL u = new URL(params[0]);
-				
+
 				HttpURLConnection conn = (HttpURLConnection) u.openConnection();
 				conn.setRequestMethod("GET");
-				
+
 				conn.connect();
 				InputStream is = conn.getInputStream();
-				
+
 				// Read the stream
 				byte[] b = new byte[1024];
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				
-				while ( is.read(b) != -1)
+
+				while (is.read(b) != -1)
 					baos.write(b);
-				
+
 				String JSONResp = new String(baos.toByteArray());
-				
+
 				JSONArray arr = new JSONArray(JSONResp);
-				for (int i=0; i < arr.length(); i++) {
+				for (int i = 0; i < arr.length(); i++) {
 					result.add(convertContact(arr.getJSONObject(i)));
 				}
-				
+
 				return result;
-			}
-			catch(Throwable t) {
+			} catch (Throwable t) {
 				t.printStackTrace();
 			}
 			return null;
 		}
-		
-		private Contact convertContact(JSONObject obj) throws JSONException {
-			String name = obj.getString("name");
-			String surname = obj.getString("surname");
-			String email = obj.getString("email");
-			String phoneNum = obj.getString("phoneNum");
-			
-			return new Contact(name, surname, email, phoneNum);
-		}
-    	
-    }
-    
-    
-    
+	}
 }
